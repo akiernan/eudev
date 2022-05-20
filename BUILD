akiernan/eudev

commit 252150e181c58c5ae58ffac144971767df9921f7

src/gudev: remove Gobject libudev support.
Signed-off-by: Anthony G. Basile <blueness@gentoo.org>

configure.ac
@@ -182,9 +182,6 @@ AC_SUBST([udevrulesdir],[${udevlibexecdir}/rules.d])
 AC_SUBST([pkgconfiglibdir], [${libdir}/pkgconfig])
 AC_SUBST([sharepkgconfigdir],[${datadir}/pkgconfig])
 
-# gudev paths
-AC_SUBST([libgudev_includedir],[${includedir}/gudev-1.0/gudev])
-
 # introspection paths
 AC_SUBST([girdir], [${datadir}/gir-1.0])
 AC_SUBST([typelibsdir], [${libdir}/girepository-1.0])
@@ -237,13 +234,6 @@ AC_CHECK_DECL([unshare],
         [#include <sched.h>])
 
 # ------------------------------------------------------------------------------
-AC_ARG_ENABLE([gudev],
-        AS_HELP_STRING([--disable-gudev], [disable Gobject libudev support @<:@default=enabled@:>@]),
-        [], [enable_gudev=yes])
-AS_IF([test "x$enable_gudev" = "xyes"], [ PKG_CHECK_MODULES([GLIB], [glib-2.0 >= 2.22.0 gobject-2.0 >= 2.22.0]) ])
-AM_CONDITIONAL([ENABLE_GUDEV], [test "x$enable_gudev" = "xyes"])
-
-# ------------------------------------------------------------------------------
 AC_PATH_TOOL(GPERF, gperf)
 if test -z "$GPERF" ; then
         AC_MSG_ERROR([*** gperf not found])
@@ -287,8 +277,6 @@ AC_CONFIG_FILES([Makefile
                  src/mtd_probe/Makefile
                  src/scsi_id/Makefile
                  src/v4l_id/Makefile
-                 src/gudev/Makefile
-                 src/gudev/gudev-1.0.pc
                  src/shared/Makefile
                  src/libudev/Makefile
                  src/libudev/libudev.pc
@@ -325,8 +313,6 @@ AC_MSG_RESULT([
 
         pkgconfiglibdir:         ${libdir}/pkgconfig
         sharepkgconfigdir        ${datadir}/pkgconfig
-
-        libgudev_includedir      ${includedir}/gudev-1.0/gudev
 
         girdir                   ${datadir}/gir-1.0
         typelibsdir              ${libdir}/girepository-1.0

.gitignore
@@ -35,13 +35,6 @@ src/mtd_probe/mtd_probe
 src/scsi_id/scsi_id
 src/v4l_id/v4l_id
 
-src/gudev/gudevenumtypes.c
-src/gudev/gudevenumtypes.h
-src/gudev/gudevmarshal.c
-src/gudev/gudevmarshal.h
-src/gudev/GUdev-1.0.gir
-src/gudev/GUdev-1.0.typelib
-
 src/udev/keyboard-keys-from-name.gperf
 src/udev/keyboard-keys-from-name.h
 src/udev/keyboard-keys-to-name.h

src/Makefile.am
@@ -13,8 +13,3 @@ SUBDIRS += \
 	mtd_probe \
 	scsi_id \
 	v4l_id
-
-if ENABLE_GUDEV
-SUBDIRS += \
-	gudev
-endif

src/gudev/Makefile.am
@@ -1,169 +1,0 @@
-ACLOCAL_AMFLAGS = -I m4 ${ACLOCAL_FLAGS}
-
-LIBGUDEV_CURRENT=2
-LIBGUDEV_REVISION=0
-LIBGUDEV_AGE=2
-
-libgudev_include_HEADERS = \
-	gudev.h \
-	gudevenums.h \
-	gudevenumtypes.h \
-	gudevtypes.h \
-	gudevclient.h \
-	gudevdevice.h \
-	gudevenumerator.h
-
-EXTRA_DIST = \
-	gudev-1.0.pc.in \
-	gudevmarshal.list \
-	gudevprivate.h \
-	gudevenumtypes.h.template \
-	gudevenumtypes.c.template \
-	gjs-example.js \
-	seed-example-enum.js \
-	seed-example.js
-
-lib_LTLIBRARIES = \
-	libgudev-1.0.la
-
-pkgconfiglib_DATA = \
-	gudev-1.0.pc
-
-CLEANFILES = \
-	gudev-1.0.pc
-
-libgudev_1_0_la_SOURCES = \
-	libgudev-1.0.sym \
-	gudevenumtypes.c\
-	gudevclient.c \
-	gudevdevice.c \
-	gudevenumerator.c
-
-nodist_libgudev_1_0_la_SOURCES = \
-	gudevmarshal.c
-
-BUILT_SOURCES = \
-	gudevenumtypes.h\
-	gudevmarshal.h \
-	$(nodist_libgudev_1_0_la_SOURCES)
-
-libgudev_1_0_la_CPPFLAGS = \
-	$(AM_CPPFLAGS) \
-	-I$(top_srcdir)/src\
-	-I$(top_builddir)/src\
-	-I$(top_srcdir)/src/libudev \
-	-I$(top_builddir)/src/gudev \
-	-I$(top_srcdir)/src/gudev \
-	-D_POSIX_PTHREAD_SEMANTICS -D_REENTRANT \
-	-D_GUDEV_COMPILATION \
-	-DG_LOG_DOMAIN=\"GUdev\"
-
-libgudev_1_0_la_CFLAGS = \
-	$(AM_CFLAGS) \
-	-fvisibility=default \
-	$(GLIB_CFLAGS)
-
-libgudev_1_0_la_LIBADD = \
-	$(top_builddir)/src/libudev/libudev.la \
-	$(GLIB_LIBS)
-
-libgudev_1_0_la_LDFLAGS = \
-	$(AM_LDFLAGS) \
-	-version-info $(LIBGUDEV_CURRENT):$(LIBGUDEV_REVISION):$(LIBGUDEV_AGE) \
-	-export-dynamic \
-	-no-undefined \
-	-Wl,--version-script=$(top_srcdir)/src/gudev/libgudev-1.0.sym
-
-gudevmarshal.h: gudevmarshal.list
-	$(AM_V_at)$(MKDIR_P) $(dir $@)
-	$(AM_V_GEN)glib-genmarshal $< --prefix=g_udev_marshal --header > $@
-
-gudevmarshal.c: gudevmarshal.list
-	$(AM_V_at)$(MKDIR_P) $(dir $@)
-	$(AM_V_GEN)echo '#include "gudevmarshal.h"' > $@ && \
-	glib-genmarshal $< --prefix=g_udev_marshal --body >> $@
-
-gudevenumtypes.%: gudevenumtypes.%.template gudevenums.h
-	$(AM_V_at)$(MKDIR_P) $(dir $@)
-	$(AM_V_GEN)glib-mkenums --template $^ > $@
-
-CLEANFILES += \
-	gudevmarshal.h \
-	gudevmarshal.c \
-	gudevenumtypes.h \
-	gudevenumtypes.c
-
-if HAVE_INTROSPECTION
--include $(INTROSPECTION_MAKEFILE)
-
-GUdev-1.0.gir: \
-	libgudev-1.0.la
-
-GUdev_1_0_gir_INCLUDES = \
-	GObject-2.0
-
-GUdev_1_0_gir_CFLAGS = \
-	$(INCLUDES) \
-	-D_GUDEV_COMPILATION \
-	-D_GUDEV_WORK_AROUND_DEV_T_BUG \
-	-I$(top_srcdir)/src \
-	-I$(top_builddir)/src \
-	-I$(top_srcdir)/src/gudev \
-	-I$(top_builddir)/src/gudev
-
-GUdev_1_0_gir_LIBS = \
-	$(top_builddir)/src/gudev/libgudev-1.0.la \
-	$(top_builddir)/src/udev/libudev-core.la
-
-GUdev_1_0_gir_SCANNERFLAGS = \
-	--pkg-export=gudev-1.0 \
-	--warn-all
-
-GUdev_1_0_gir_FILES = \
-	gudev.h \
-	gudevtypes.h \
-	gudevenums.h \
-	gudevenumtypes.h \
-	gudevclient.h \
-	gudevdevice.h \
-	gudevenumerator.h \
-	gudevclient.c \
-	gudevdevice.c \
-	gudevenumerator.c
-
-INTROSPECTION_GIRS = \
-	GUdev-1.0.gir
-
-INTROSPECTION_SCANNER_ARGS = \
-	--c-include=gudev.h
-
-gir_DATA = \
-	GUdev-1.0.gir
-
-typelibs_DATA = \
-	GUdev-1.0.typelib
-
-CLEANFILES += \
-	$(gir_DATA) $(typelibs_DATA)
-
-endif # HAVE_INTROSPECTION
-
-# move lib from $(libdir) to $(rootlibdir) and update devel link, if needed
-libgudev-install-move-hook:
-	if test "$(libdir)" != "$(rootlibdir)"; then \
-		$(MKDIR_P) $(DESTDIR)$(rootlibdir) && \
-		so_img_name=$$(readlink $(DESTDIR)$(libdir)/libgudev-1.0.so) && \
-		so_img_rel_target_prefix=$$(echo $(libdir) | sed 's,\(^/\|\)[^/][^/]*,..,g') && \
-		ln -sf $$so_img_rel_target_prefix$(rootlibdir)/$$so_img_name $(DESTDIR)$(libdir)/libgudev-1.0.so && \
-		mv $(DESTDIR)$(libdir)/libgudev-1.0.so.* $(DESTDIR)$(rootlibdir); \
-	fi
-
-libgudev-uninstall-move-hook:
-	rm -f $(DESTDIR)$(rootlibdir)/libgudev-1.0.so*
-
-INSTALL_EXEC_HOOKS = \
-	libgudev-install-move-hook
-
-UNINSTALL_EXEC_HOOKS = \
-	libgudev-uninstall-move-hook
-

src/gudev/gjs-example.js
@@ -1,75 +1,0 @@
-#!/usr/bin/env gjs-console
-
-// This currently depends on the following patches to gjs
-//
-// http://bugzilla.gnome.org/show_bug.cgi?id=584558
-// http://bugzilla.gnome.org/show_bug.cgi?id=584560
-// http://bugzilla.gnome.org/show_bug.cgi?id=584568
-
-const GUdev = imports.gi.GUdev;
-const Mainloop = imports.mainloop;
-
-function print_device (device) {
-  print ("  subsystem:             " + device.get_subsystem ());
-  print ("  devtype:               " + device.get_devtype ());
-  print ("  name:                  " + device.get_name ());
-  print ("  number:                " + device.get_number ());
-  print ("  sysfs_path:            " + device.get_sysfs_path ());
-  print ("  driver:                " + device.get_driver ());
-  print ("  action:                " + device.get_action ());
-  print ("  seqnum:                " + device.get_seqnum ());
-  print ("  device type:           " + device.get_device_type ());
-  print ("  device number:         " + device.get_device_number ());
-  print ("  device file:           " + device.get_device_file ());
-  print ("  device file symlinks:  " + device.get_device_file_symlinks ());
-  print ("  foo: " + device.get_sysfs_attr_as_strv ("stat"));
-  var keys = device.get_property_keys ();
-  for (var n = 0; n < keys.length; n++) {
-    print ("    " + keys[n] + "=" + device.get_property (keys[n]));
-  }
-}
-
-function on_uevent (client, action, device) {
-  print ("action " + action + " on device " + device.get_sysfs_path());
-  print_device (device);
-  print ("");
-}
-
-var client = new GUdev.Client ({subsystems: ["block", "usb/usb_interface"]});
-client.connect ("uevent", on_uevent);
-
-var block_devices = client.query_by_subsystem ("block");
-for (var n = 0; n < block_devices.length; n++) {
-  print ("block device: " + block_devices[n].get_device_file ());
-}
-
-var d;
-
-d = client.query_by_device_number (GUdev.DeviceType.BLOCK, 0x0810);
-if (d == null) {
-  print ("query_by_device_number 0x810 -> null");
-} else {
-  print ("query_by_device_number 0x810 -> " + d.get_device_file ());
-  var dd = d.get_parent_with_subsystem ("usb", null);
-  print_device (dd);
-  print ("--------------------------------------------------------------------------");
-  while (d != null) {
-    print_device (d);
-    print ("");
-    d = d.get_parent ();
-  }
-}
-
-d = client.query_by_sysfs_path ("/sys/block/sda/sda1");
-print ("query_by_sysfs_path (\"/sys/block/sda1\") -> " + d.get_device_file ());
-
-d = client.query_by_subsystem_and_name ("block", "sda2");
-print ("query_by_subsystem_and_name (\"block\", \"sda2\") -> " + d.get_device_file ());
-
-d = client.query_by_device_file ("/dev/sda");
-print ("query_by_device_file (\"/dev/sda\") -> " + d.get_device_file ());
-
-d = client.query_by_device_file ("/dev/block/8:0");
-print ("query_by_device_file (\"/dev/block/8:0\") -> " + d.get_device_file ());
-
-Mainloop.run('udev-example');

src/gudev/gudev-1.0.pc.in
@@ -1,11 +1,0 @@
-prefix=@prefix@
-exec_prefix=@exec_prefix@
-libdir=@libdir@
-includedir=@includedir@
-
-Name: gudev-1.0
-Description: GObject bindings for libudev
-Version: @UDEV_VERSION@
-Requires: glib-2.0, gobject-2.0
-Libs: -L${libdir} -lgudev-1.0
-Cflags: -I${includedir}/gudev-1.0

src/gudev/gudev.h
@@ -1,32 +1,0 @@
-/* -*- Mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*-
- *
- * Copyright (C) 2008 David Zeuthen <davidz@redhat.com>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
-
-#ifndef __G_UDEV_H__
-#define __G_UDEV_H__
-
-#define _GUDEV_INSIDE_GUDEV_H
-#include <gudev/gudevenums.h>
-#include <gudev/gudevenumtypes.h>
-#include <gudev/gudevtypes.h>
-#include <gudev/gudevclient.h>
-#include <gudev/gudevdevice.h>
-#include <gudev/gudevenumerator.h>
-#undef _GUDEV_INSIDE_GUDEV_H
-
-#endif /* __G_UDEV_H__ */

commit b961d1255b31cfdb9815826d8e29a4ed4c284830

build: Remove dead g-i-r configuration
g-i-r support was removed in 2015 as part of removal of Gobject libudev
support, but introspection discovery wasn't removed but is dead.

Fixes: 252150e181c5 ("src/gudev: remove Gobject libudev support.")

BUILD
@@ -27,8 +27,6 @@ the Linux distribution:
       autoconf \
       automake \
       build-base \
-      glib-dev \
-      gobject-introspection-dev \
       gperf \
       kmod-dev \
       libxslt \
@@ -67,13 +65,11 @@ the Linux distribution:
           docbook-xml \
           docbook-xsl \
           gnu-efi \
-          gobject-introspection \
           gperf \
           intltool \
           libacl1-dev \
           libblkid-dev \
           libcap-dev \
-          libglib-object-introspection-perl \
           libkmod-dev \
           libselinux1-dev \
           libtool \
@@ -89,7 +85,6 @@ the Linux distribution:
     docbook-xml-dtd \
     docbook-xml-dtd \
     docbook-xsl-stylesheets \
-    gobject-introspection \
     gperf \
     kmod \
     libselinux \

configure.ac
@@ -183,16 +183,7 @@ AC_SUBST([udevrulesdir],[${udevlibexecdir}/rules.d])
 AC_SUBST([pkgconfiglibdir], [${libdir}/pkgconfig])
 AC_SUBST([sharepkgconfigdir],[${datadir}/pkgconfig])
 
-# introspection paths
-AC_SUBST([girdir], [${datadir}/gir-1.0])
-AC_SUBST([typelibsdir], [${libdir}/girepository-1.0])
-
 AC_SUBST([rootrundir],[${with_rootrundir}])
-
-# ------------------------------------------------------------------------------
-
-GOBJECT_INTROSPECTION_CHECK([1.31.1])
-AM_CONDITIONAL([HAVE_INTROSPECTION], [test "$enable_introspection" = "yes"])
 
 # ------------------------------------------------------------------------------
 AC_ARG_ENABLE([programs],
@@ -349,9 +340,6 @@ AC_MSG_RESULT([
 
 	pkgconfiglibdir:         ${libdir}/pkgconfig
 	sharepkgconfigdir        ${datadir}/pkgconfig
-
-	girdir                   ${datadir}/gir-1.0
-	typelibsdir              ${libdir}/girepository-1.0
 ])
 
 # ------------------------------------------------------------------------------

m4/introspection.m4
@@ -1,96 +1,0 @@
-dnl -*- mode: autoconf -*-
-dnl Copyright 2009 Johan Dahlin
-dnl
-dnl This file is free software; the author(s) gives unlimited
-dnl permission to copy and/or distribute it, with or without
-dnl modifications, as long as this notice is preserved.
-dnl
-
-# serial 1
-
-m4_define([_GOBJECT_INTROSPECTION_CHECK_INTERNAL],
-[
-    AC_BEFORE([AC_PROG_LIBTOOL],[$0])dnl setup libtool first
-    AC_BEFORE([AM_PROG_LIBTOOL],[$0])dnl setup libtool first
-    AC_BEFORE([LT_INIT],[$0])dnl setup libtool first
-
-    dnl enable/disable introspection
-    m4_if([$2], [require],
-    [dnl
-        enable_introspection=yes
-    ],[dnl
-        AC_ARG_ENABLE(introspection,
-                  AS_HELP_STRING([--enable-introspection[=@<:@no/auto/yes@:>@]],
-                                 [Enable introspection for this build]),, 
-                                 [enable_introspection=auto])
-    ])dnl
-
-    AC_MSG_CHECKING([for gobject-introspection])
-
-    dnl presence/version checking
-    AS_CASE([$enable_introspection],
-    [no], [dnl
-        found_introspection="no (disabled, use --enable-introspection to enable)"
-    ],dnl
-    [yes],[dnl
-        PKG_CHECK_EXISTS([gobject-introspection-1.0],,
-                         AC_MSG_ERROR([gobject-introspection-1.0 is not installed]))
-        PKG_CHECK_EXISTS([gobject-introspection-1.0 >= $1],
-                         found_introspection=yes,
-                         AC_MSG_ERROR([You need to have gobject-introspection >= $1 installed to build AC_PACKAGE_NAME]))
-    ],dnl
-    [auto],[dnl
-        PKG_CHECK_EXISTS([gobject-introspection-1.0 >= $1], found_introspection=yes, found_introspection=no)
-	dnl Canonicalize enable_introspection
-	enable_introspection=$found_introspection
-    ],dnl
-    [dnl	
-        AC_MSG_ERROR([invalid argument passed to --enable-introspection, should be one of @<:@no/auto/yes@:>@])
-    ])dnl
-
-    AC_MSG_RESULT([$found_introspection])
-
-    INTROSPECTION_SCANNER=
-    INTROSPECTION_COMPILER=
-    INTROSPECTION_GENERATE=
-    INTROSPECTION_GIRDIR=
-    INTROSPECTION_TYPELIBDIR=
-    if test "x$found_introspection" = "xyes"; then
-       INTROSPECTION_SCANNER=`$PKG_CONFIG --variable=g_ir_scanner gobject-introspection-1.0`
-       INTROSPECTION_COMPILER=`$PKG_CONFIG --variable=g_ir_compiler gobject-introspection-1.0`
-       INTROSPECTION_GENERATE=`$PKG_CONFIG --variable=g_ir_generate gobject-introspection-1.0`
-       INTROSPECTION_GIRDIR=`$PKG_CONFIG --variable=girdir gobject-introspection-1.0`
-       INTROSPECTION_TYPELIBDIR="$($PKG_CONFIG --variable=typelibdir gobject-introspection-1.0)"
-       INTROSPECTION_CFLAGS=`$PKG_CONFIG --cflags gobject-introspection-1.0`
-       INTROSPECTION_LIBS=`$PKG_CONFIG --libs gobject-introspection-1.0`
-       INTROSPECTION_MAKEFILE=`$PKG_CONFIG --variable=datadir gobject-introspection-1.0`/gobject-introspection-1.0/Makefile.introspection
-    fi
-    AC_SUBST(INTROSPECTION_SCANNER)
-    AC_SUBST(INTROSPECTION_COMPILER)
-    AC_SUBST(INTROSPECTION_GENERATE)
-    AC_SUBST(INTROSPECTION_GIRDIR)
-    AC_SUBST(INTROSPECTION_TYPELIBDIR)
-    AC_SUBST(INTROSPECTION_CFLAGS)
-    AC_SUBST(INTROSPECTION_LIBS)
-    AC_SUBST(INTROSPECTION_MAKEFILE)
-
-    AM_CONDITIONAL(HAVE_INTROSPECTION, test "x$found_introspection" = "xyes")
-])
-
-
-dnl Usage:
-dnl   GOBJECT_INTROSPECTION_CHECK([minimum-g-i-version])
-
-AC_DEFUN([GOBJECT_INTROSPECTION_CHECK],
-[
-  _GOBJECT_INTROSPECTION_CHECK_INTERNAL([$1])
-])
-
-dnl Usage:
-dnl   GOBJECT_INTROSPECTION_REQUIRE([minimum-g-i-version])
-
-
-AC_DEFUN([GOBJECT_INTROSPECTION_REQUIRE],
-[
-  _GOBJECT_INTROSPECTION_CHECK_INTERNAL([$1], [require])
-])